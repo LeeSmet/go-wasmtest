604ee02e827e6ebb2cead51a3c61b1fb7eeba7d9
LeeSmet <lee.smet@hotmail.com>
2019-03-06 13:36:29 +0100

    Simple webpage rendering

--- Makefile
+++ Makefile
@@ -2,3 +2,8 @@ all: build
 
 build:
 	GOOS=js GOARCH=wasm go build -o main.wasm
+
+jshelper:
+	cp $(shell go env GOROOT)/misc/wasm/wasm_exec.js .
+
+.PHONY: build
--- main.go
+++ main.go
@@ -1,7 +1,51 @@
+// +build js
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"syscall/js"
+)
+
+var wallets = []string{"wallet1", "wallet2", "wallet3"}
 
 func main() {
 	fmt.Println("Hello wasm")
+
+	window := js.Global()
+	if window == js.Null() {
+		fmt.Println("Failed to get a handle on the window")
+		return
+	}
+
+	document := window.Get("document")
+	if document == js.Null() {
+		fmt.Println("Failed to get a handle on the document")
+		return
+	}
+
+	title := document.Call("getElementById", "title")
+	if title == js.Null() {
+		fmt.Println("Failed to get a reference to the title")
+		return
+	}
+
+	title.Set("innerText", "Hello from go wasm")
+
+	body := document.Get("body")
+	if body == js.Null() {
+		fmt.Println("Failed to get body")
+	}
+
+	currentBody := body.Get("innerHTML").String()
+
+	walletList := "<ul>\n"
+	for _, walletName := range wallets {
+		walletList += fmt.Sprintf("<li>%v</li>\n", walletName)
+	}
+	walletList += "</ul>\n"
+
+	currentBody += walletList
+	body.Set("innerHTML", currentBody)
 }
